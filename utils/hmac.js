743743f8f8305d9b270598375c266e2b06b1fc90
cresmo27 <cresmo27@gmail.com>
2025-10-16 00:21:11 +0200

    Delete utils directory

--- utils/hmac.js
+++
@@ -1,7 +0,0 @@
-import crypto from "crypto";
-
-const { LICENSE_SECRET = "dev_license_secret" } = process.env;
-
-export function hmacHex(input) {
-  return crypto.createHmac("sha256", LICENSE_SECRET).update(input).digest("hex");
-}
